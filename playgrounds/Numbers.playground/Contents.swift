20a46026a320f64f396f7b66053c29fb28de28fe
Jeff Eng <jeff@jeffeng.me>
2016-12-06 16:51:53 -0800

    Finished lecture on numbers practicing number types and arithmetic operators.

---
+++ playgrounds/Numbers.playground/Contents.swift
@@ -0,0 +1,41 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Numbers
+// Type inference
+var age = 30
+
+// Explicitly declared type
+var weight: Int = 200 // Int
+
+var milesRan = 56.45
+
+var pi: Float = 3.14
+
+
+
+//Arithmetic Operators
+// + - / *
+
+var area = 15 * 20
+
+var sum = 5 + 6
+
+var diff = 10 - 3
+
+var div = 12 / 3
+
+var div1 = 13 / 5
+
+var remainder = 13 % 5
+
+var result = "The result of 13 / 5 is \(div1) with a remainder of \(remainder)"
+
+var randomNumber = 13
+
+if randomNumber % 2 == 0 {
+    print("The random number is even")
+} else {
+    print("The random number is odd")
+}
