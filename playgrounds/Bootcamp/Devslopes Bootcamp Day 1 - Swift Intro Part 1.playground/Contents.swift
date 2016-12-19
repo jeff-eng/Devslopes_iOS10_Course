e436bb850673f063b6d94fbdef37a05dd563f535
Jeff Eng <jeff@jeffeng.me>
2016-12-19 14:02:57 -0800

    finished the exercise from the bootcamp, same as the variables exercise done previously

---
+++ playgrounds/Bootcamp/Devslopes Bootcamp Day 1 - Swift Intro Part 1.playground/Contents.swift
@@ -0,0 +1,52 @@
+// Devslopes Bootcamp Day 1 - Swift Intro Part 1
+
+import UIKit
+
+print("Hello, Jeff!")
+
+//var age = 25 //Type Inference - Swift infers the type
+//var age2: Int //Explicitly-defined type
+//age2 = 35
+//print(age2)
+//
+//var firstName = "Jimmy"
+//var lastName = "John"
+//
+//var fullName = firstName + " " + lastName
+//
+//var length = 10
+//var width = 50
+//
+//let area = length * width
+//
+//var badMath = length + width
+//var badMath2 = "10" + "50"
+//
+//var phrase = "Hi, my name is \(firstName) \(lastName)" //String Interpolation
+
+var apples = 15
+
+var oranges = 5
+
+var fruit = apples + oranges
+
+var product = 8 * 8
+
+var difference = apples - oranges
+
+var division = apples / oranges
+
+var dogs = 1, cats = 2, mice = 3
+
+let beer = 4, liquor = 5, wine = 6
+
+var rainMeasurement: Double
+
+var attendance: Int = 423
+
+var helloString = "Hello"
+var worldString = "World"
+
+var combinedString = helloString + " " + worldString
+
+var combinedString2 = "\(helloString) \(worldString)"
