f80d32fa9ef89b6c4ecd3d616d2bf151c7f85794
Jeff Eng <jeff@jeffeng.me>
2016-12-21 15:11:47 -0800

    Made the ViewController class the delegate and the datasource for the statePicker per the protocol requirements. Also added in the required methods for the UIPickerViewDataSource protocol. Created an array for the states.

--- Foundational iOS/MiraclePills/MiraclePill/MiraclePill/ViewController.swift
+++ Foundational iOS/MiraclePills/MiraclePill/MiraclePill/ViewController.swift
@@ -8,14 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
     @IBOutlet weak var statePicker: UIPickerView!
     @IBOutlet weak var statePickerBtn: UIButton!
+
+    // Create a list of states using an array (an array is the most simple and logical data structure to organize a list of states)
+    let states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        statePicker.dataSource = self
+        statePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,5 +30,18 @@ class ViewController: UIViewController {
     @IBAction func stateBtnPressed(_ sender: Any) {
     }
 
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        // Returning 1 because we only want a single(1) column of states to pick through.
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        // return the number of states in our list of states (the array)
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
 }
 
