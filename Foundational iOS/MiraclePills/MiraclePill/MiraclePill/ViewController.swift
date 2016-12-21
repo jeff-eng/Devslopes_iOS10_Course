ce0aa8aa4611c9c11a14f5ebd44f991bb13a9155
Jeff Eng <jeff@jeffeng.me>
2016-12-21 14:07:31 -0800

    Added a Button and a Picker View to the scene. Created outlets for the button and Picker View, and also an IBAction for the Button being pressed.

--- Foundational iOS/MiraclePills/MiraclePill/MiraclePill/ViewController.swift
+++ Foundational iOS/MiraclePills/MiraclePill/MiraclePill/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func stateBtnPressed(_ sender: Any) {
+    }
 
 }
 
