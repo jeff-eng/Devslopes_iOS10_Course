f8de4e9eb82fcf122bf3841989bf0dec992720b0
Jeff Eng <jeff@jeffeng.me>
2016-12-28 14:43:30 -0800

    created initial project files for the stack view app

---
+++ Foundational iOS/Stack-a-View/Stack-a-View/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stack-a-View
+//
+//  Created by Jeffrey Eng on 12/28/16.
+//  Copyright © 2016 Jeffrey Eng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
