f4c39edeb853acf23b38779f42175e39e488a48b
Jeff Eng <jeff@jeffeng.me>
2016-12-28 11:17:03 -0800

    Added a scroll view to the app and enabled swiping to scroll horizontally to see different icons.

--- Foundational iOS/PageTheScroll/PageTheScroll/ViewController.swift
+++ Foundational iOS/PageTheScroll/PageTheScroll/ViewController.swift
@@ -10,15 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    var images = [UIImageView]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var contentWidth: CGFloat = 0.0
+        
+        for x in 0...2 {
+            let image = UIImage(named: "icon\(x).png")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            
+            newX = view.frame.midX + view.frame.size.width * CGFloat(x)
+            
+            contentWidth += newX
+            
+            // Add the image view as a subview
+            scrollView.addSubview(imageView)
+            
+            // Set the frame on our image view which includes positioning and size
+            imageView.frame = CGRect(x: newX - 75, y: (view.frame.size.height / 2) - 75, width: 150, height: 150)
+        }
+        
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
