363a67c5e5b7e82548897d82d40e6fec095cbd6d
Jeff Eng <jeff@jeffeng.me>
2016-12-30 15:30:35 -0800

    Created a new View Controller class, View Controllers in storyboard and also created a segue from login button to the new View Controller.

---
+++ Foundational iOS/SwappingScreens/SwappingScreens/LoginVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwappingScreens
+//
+//  Created by Jeffrey Eng on 12/30/16.
+//  Copyright © 2016 Jeffrey Eng. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
---
+++ Foundational iOS/SwappingScreens/SwappingScreens/MusicListVC.swift
@@ -0,0 +1,30 @@
+//
+//  MusicListVC.swift
+//  SwappingScreens
+//
+//  Created by Jeffrey Eng on 12/30/16.
+//  Copyright © 2016 Jeffrey Eng. All rights reserved.
+//
+
+import UIKit
+
+class MusicListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.blue
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
