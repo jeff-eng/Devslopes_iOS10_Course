4a7f447a75501d986475f3e0cb97f3e431b03c8a
Jeff Eng <jeff@jeffeng.me>
2017-01-09 16:23:26 -0800

    Created UI with banner view, buttons, and table view with prototype cells. Also created a second view controller along with a segue from MainVC to the VideoVC and added a web view to that VideoVC.

---
+++ Foundational iOS/PartyRockApp/PartyRockApp/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Jeffrey Eng on 1/9/17.
+//  Copyright © 2017 Jeffrey Eng. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
