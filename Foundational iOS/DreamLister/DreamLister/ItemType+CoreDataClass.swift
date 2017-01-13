c8c84c896818299d0d3cbb67954dbbdc6d6a0e11
Jeff Eng <jeff@jeffeng.me>
2017-01-12 16:10:09 -0800

    Created the data model using Core Data. Created entities and defined the relationships. Also created the NSManagedObject subclass. Unfortunately did not create a git branch for the data modeling.

---
+++ Foundational iOS/DreamLister/DreamLister/ItemType+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ItemType+CoreDataClass.swift
+//  DreamLister
+//
+//  Created by Jeffrey Eng on 1/12/17.
+//  Copyright © 2017 Jeffrey Eng. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class ItemType: NSManagedObject {
+
+}
