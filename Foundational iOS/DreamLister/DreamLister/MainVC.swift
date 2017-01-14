fa65a364dbdd6c509f007d1b3fa71771382c1c95
Jeff Eng <jeff@jeffeng.me>
2017-01-13 22:18:57 -0800

    Configured cells to display data. Also persisted the data from the context and saved to the database.

--- Foundational iOS/DreamLister/DreamLister/MainVC.swift
+++ Foundational iOS/DreamLister/DreamLister/MainVC.swift
@@ -21,18 +21,50 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        //Generate our test data for purpose of testing the app
+//        generateTestData()
+        
+        attemptFetch()
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
+        configureCell(cell: cell, indexPath: indexPath as NSIndexPath)
+        
+        return cell
+    }
+    
+    func configureCell(cell: ItemCell, indexPath: NSIndexPath) {
+        
+        let item = controller.object(at: indexPath as IndexPath)
+        // Calls configureCell method in the custom ItemCell class
+        cell.configureCell(item: item)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // If there are sections, then get a reference to it
+        if let sections = controller.sections {
+            // Get the info out of the section
+            let sectionInfo = sections[section]
+            // Return the count of them
+            return sectionInfo.numberOfObjects
+        }
+        // If there aren't any, then return zero
         return 0
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
+        
+        if let sections = controller.sections {
+            return sections.count
+        }
         return 0
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
     }
     
     // Once we have data in our database, we need a way to fetch it and display it
@@ -46,6 +78,8 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         
         //Instantiate the Fetched Results Controller - pass in which fetch request we're working with(fetching Items); context comes from app delegate
         let controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        
+        self.controller = controller
         
         // Perform Fetch - A fetch request can fail, so we need to implement error handling with a do-catch statement
         do {
@@ -85,7 +119,7 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         case.update:
             if let indexPath = indexPath {
                 let cell = tableView.cellForRow(at: indexPath) as! ItemCell
-                // update the cell data.
+                configureCell(cell: cell, indexPath: indexPath as NSIndexPath)
             }
             break
         case.move:
@@ -99,6 +133,26 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
             }
             break
         }
+    }
+    
+    func generateTestData() {
+        let item = Item(context: context)
+        item.title = "MacBook Pro"
+        item.price = 1800
+        item.details = "I can't wait until the September event, I hope they release new MBPs"
+        
+        let item2 = Item(context: context)
+        item2.title = "Dell Ultrasharp Monitor"
+        item2.price = 400
+        item2.details = "Need me a new monitor so I can run Xcode on a bigger screen!"
+        
+        let item3 = Item(context: context)
+        item3.title = "LG OLED TV"
+        item3.price = 2500
+        item3.details = "Best TV on the market. 4K and HDR make me drool."
+        
+        // Calling this app delegate method will save it to the database
+        ad.saveContext()
     }
 }
 
