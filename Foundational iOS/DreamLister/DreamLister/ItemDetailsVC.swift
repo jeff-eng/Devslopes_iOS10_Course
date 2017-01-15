754b3aabaae4a55c6ef430bf525db3907ca2be08
Jeff Eng <jeff@jeffeng.me>
2017-01-15 12:21:42 -0800

    Created loadItemData method to handle loading existing item data.

--- Foundational iOS/DreamLister/DreamLister/ItemDetailsVC.swift
+++ Foundational iOS/DreamLister/DreamLister/ItemDetailsVC.swift
@@ -19,6 +19,9 @@ class ItemDetailsVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSou
     // Array to hold a list of the Store entities
     var stores = [Store]()
     
+    // Property to save and reference current item being edited.
+    var itemToEdit: Item?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -52,6 +55,11 @@ class ItemDetailsVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSou
 //        
 //        ad.saveContext()
         getStores()
+        
+        // Load the Item data if there is an item to edit
+        if itemToEdit != nil {
+            loadItemData()
+        }
     }
 
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
@@ -111,5 +119,26 @@ class ItemDetailsVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSou
         
         // After save button is pressed, dismiss VC and pop it off navigation stack
         _ = navigationController?.popViewController(animated: true)
+    }
+    
+    // Method to load up existing item data
+    func loadItemData() {
+        if let item = itemToEdit {
+            titleField.text = item.title
+            priceField.text = "\(item.price)"
+            detailsField.text = item.details
+            
+            if let store = item.toStore {
+                var index = 0
+                repeat {
+                    let s = stores[index]
+                    if s.name == store.name {
+                        storePicker.selectRow(index, inComponent: 0, animated: false)
+                        break
+                    }
+                    index += 1
+                } while index < stores.count
+            }
+        }
     }
 }
