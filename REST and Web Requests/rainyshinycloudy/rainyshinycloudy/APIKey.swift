5a77877405e94fe3ff6680d814a7839c4801f6e8
Jeff Eng <jeff@jeffeng.me>
2017-01-24 16:04:04 -0800

    Put API key in a separate file and added the file to gitignore. Added a gitignore file to this project.

---
+++ REST and Web Requests/rainyshinycloudy/rainyshinycloudy/APIKey.swift
@@ -0,0 +1,11 @@
+//
+//  APIKey.swift
+//  rainyshinycloudy
+//
+//  Created by Jeffrey Eng on 1/24/17.
+//  Copyright © 2017 Jeffrey Eng. All rights reserved.
+//
+
+import Foundation
+
+let API_KEY = "81151a9300b500142e19bf5a1e796e3c"
--- REST and Web Requests/rainyshinycloudy/rainyshinycloudy/Constants.swift
+++ REST and Web Requests/rainyshinycloudy/rainyshinycloudy/Constants.swift
@@ -12,7 +12,6 @@ let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
 let LATITUDE = "lat="
 let LONGITUDE = "&lon="
 let APP_ID = "&appid="
-let API_KEY = "81151a9300b500142e19bf5a1e796e3c"
 
 let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITUDE)-36\(LONGITUDE)123\(APP_ID)\(API_KEY)"
 
