7b58674a518c1dc5a46c354fa88b562b5774ab06
Jeff Eng <jeff@jeffeng.me>
2017-01-24 16:38:29 -0800

    Created the CurrentWeather class for our data model. Used private variables.

---
+++ REST and Web Requests/rainyshinycloudy/rainyshinycloudy/CurrentWeather.swift
@@ -0,0 +1,56 @@
+//
+//  CurrentWeather.swift
+//  rainyshinycloudy
+//
+//  Created by Jeffrey Eng on 1/24/17.
+//  Copyright © 2017 Jeffrey Eng. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class CurrentWeather {
+    
+    // MARK: Private Variables
+    private var _cityName: String!
+    private var _date: String!
+    private var _weatherType: String!
+    private var _currentTemp: Double!
+    
+    // MARK: Getters
+    // Best practice - Using data encapsulation here to prevent outside classes from manipulating data.
+    var cityName: String {
+        if _cityName == nil {
+            _cityName = ""
+        }
+        return _cityName
+    }
+    
+    var date: String {
+        if _date == nil {
+            _date = ""
+        }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .none
+        let currentDate = dateFormatter.string(from: Date())
+        self._date = "Today, \(currentDate)"
+        
+        return _date
+    }
+    
+    var weatherType: String {
+        if _weatherType == nil {
+            _weatherType = ""
+        }
+        return _weatherType
+    }
+    
+    var currentTemp: Double {
+        if _currentTemp == nil {
+            _currentTemp = 0.0
+        }
+        return _currentTemp
+    }
+}
