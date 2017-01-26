bc3727bf13b350c383f1f34a3010c90a474e8b42
Jeff Eng <jeff@jeffeng.me>
2017-01-25 20:40:54 -0800

    Updated the AppDelegate.swift file with the updated didFinishLaunchingWithOptions method.

--- REST and Web Requests/rainyshinycloudy/rainyshinycloudy/AppDelegate.swift
+++ REST and Web Requests/rainyshinycloudy/rainyshinycloudy/AppDelegate.swift
@@ -12,12 +12,17 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    
+    // This is the updated didFinishLaunchingWithOptions application method.
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
         return true
     }
+    
+    // This is the old method
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+//        // Override point for customization after application launch.
+//        return true
+//    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
