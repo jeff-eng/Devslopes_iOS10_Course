dafd0346982e7c94a4d2ed46c72c3257c5d91521
Jeff Eng <jeff@jeffeng.me>
2017-01-27 12:11:48 -0800

    Added a custom table view cell file, connected the UI elements to code as IBOutlets and added a method to update the text in the labels of the table view cell to our data.

---
+++ REST and Web Requests/rainyshinycloudy/rainyshinycloudy/WeatherCell.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherCell.swift
+//  rainyshinycloudy
+//
+//  Created by Jeffrey Eng on 1/27/17.
+//  Copyright © 2017 Jeffrey Eng. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCell: UITableViewCell {
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var weatherTypeLabel: UILabel!
+    @IBOutlet weak var highTempLabel: UILabel!
+    @IBOutlet weak var lowTempLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    func configureCell(forecast: Forecast) {
+        lowTempLabel.text = forecast.lowTemp
+        highTempLabel.text = forecast.highTemp
+        weatherTypeLabel.text = forecast.weatherType
+        weatherIcon.image = UIImage(named: forecast.weatherType)
+        dayLabel.text = forecast.date
+    }
+}
