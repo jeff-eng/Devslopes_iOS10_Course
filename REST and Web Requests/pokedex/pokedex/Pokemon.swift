7be767ca8898e758801614ec32e6622bca68e097
Jeff Eng <jeff@jeffeng.me>
2017-01-31 14:09:04 -0800

    Created a new Pokemon class and added the private variables, public variables, and initializer. Also have a Model folder for MVC design pattern and code organization.

---
+++ REST and Web Requests/pokedex/pokedex/Pokemon.swift
@@ -0,0 +1,30 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Jeffrey Eng on 1/31/17.
+//  Copyright © 2017 Jeffrey Eng. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    // MARK: Private variables
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    // MARK: Class Properties
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    // MARK: Initializer
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
