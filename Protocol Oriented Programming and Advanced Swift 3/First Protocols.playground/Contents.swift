7611407a1a19b38f9ad42ad0fab6390cc90b28d9
Jeff Eng <jeff@jeffeng.me>
2017-02-10 12:05:39 -0800

    Added creating protocol extensions and practice using them.

--- Protocol Oriented Programming and Advanced Swift 3/First Protocols.playground/Contents.swift
+++ Protocol Oriented Programming and Advanced Swift 3/First Protocols.playground/Contents.swift
@@ -4,21 +4,19 @@ import UIKit
 
 protocol Vehicle: CustomStringConvertible{
     var isRunning: Bool { get set }
+    var make: String {  get set }
+    var model: String { get set }
     
     mutating func start()
     
     mutating func turnOff()
+    
 }
 
-//Creating a sports car Structure that conforms to the Vehicle protocol
-struct SportsCar: Vehicle {
-    var isRunning: Bool = false
-    var description: String {
-        if isRunning {
-           return "Sports car currently running"
-        } else {
-           return "Sports car currently turned off"
-        }
+extension Vehicle {
+    
+    var makeModel: String {
+        return "\(make) \(model)"
     }
     
     mutating func start() {
@@ -26,46 +24,46 @@ struct SportsCar: Vehicle {
             print("Already started")
         } else {
             isRunning = true
-            print("Vrooom")
+            print("\(self.description) fired up!")
         }
     }
     
     mutating func turnOff() {
         if isRunning {
             isRunning = false
-            print("Crickets")
+            print("\(self.description) shut down!")
         } else {
             print("Already dead!")
         }
     }
 }
 
+//Creating a sports car Structure that conforms to the Vehicle protocol
+struct SportsCar: Vehicle {
+    var isRunning: Bool = false
+    var make: String
+    var model: String
+    
+    var description: String {
+        return self.makeModel
+    }
+    
+}
+
 //Now creating a SemiTruck class that conforms to the Vehicle protocol to show that protocols work with both value and reference types
 class SemiTruck: Vehicle {
     var isRunning: Bool = false
-    var description: String {
-        if isRunning {
-            return "Semi truck currently running"
-        } else {
-            return "Semi truck currently shut down"
-        }
-    }
-    func start() {
-        if isRunning {
-            print("Already started")
-        } else {
-            isRunning = true
-            print("Rumble")
-        }
+    var make: String
+    var model: String
+    
+    init(isRunning: Bool, make: String, model: String) {
+        self.isRunning = isRunning
+        self.make = make
+        self.model = model
     }
     
-    func turnOff() {
-        if isRunning {
-            isRunning = false
-            print("put...put...silence")
-        } else {
-            print("Already shut down")
-        }
+    var description: String {
+        return self.makeModel
     }
     
     // You can specify additional functionality not specified by the protocol.
@@ -75,20 +73,33 @@ class SemiTruck: Vehicle {
     
 }
 
-var car1 = SportsCar()
-var truck1 = SemiTruck()
-
+var car1 = SportsCar(isRunning: false, make: "Porsche", model: "911")
+var truck1 = SemiTruck(isRunning: false, make: "Peterbilt", model: "Verago")
+//
 car1.start()
 truck1.start()
 truck1.blowAirHorn()
+
+truck1.turnOff()
 
 // Creating an array to store the SportsCar instance and SemiTruck instance; even though they are different types (Struct vs. Class), you can store both of them in the array because they both conform to Vehicle protocol. This shows you can use protocols as types.
 var vehicleArray: Array<Vehicle> = [car1, truck1]
 
 
 for vehicle in vehicleArray {
-    print("\(vehicle.description)")
+    print("\(vehicle.makeModel)")
 }
 
+
+// Creating Protocol Extensions
+
+extension Double {
+    var dollarString: String {
+        return String(format: "$%.02f", self)
+    }
+}
+
+var pct = 32.15 * 0.15  // returns 4.8225
+pct.dollarString        // returns "$4.82"
 
 
