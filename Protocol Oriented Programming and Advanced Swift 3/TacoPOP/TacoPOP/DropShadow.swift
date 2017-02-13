65c08958800aca9252ce72f7e6dc71120c741dac
Jeff Eng <jeff@jeffeng.me>
2017-02-13 11:34:25 -0800

    Created new project TacoPOP and added folders for project organization. Started the storyboard and added a few UI elements such as header and logo. Just added a new swift file for a Protocol that is being used to add a drop shadow to the logo.

---
+++ Protocol Oriented Programming and Advanced Swift 3/TacoPOP/TacoPOP/DropShadow.swift
@@ -0,0 +1,13 @@
+//
+//  DropShadow.swift
+//  TacoPOP
+//
+//  Created by Jeffrey Eng on 2/13/17.
+//  Copyright © 2017 Jeffrey Eng. All rights reserved.
+//
+
+import UIKit
+
+protocol DropShadow {
+    
+}
