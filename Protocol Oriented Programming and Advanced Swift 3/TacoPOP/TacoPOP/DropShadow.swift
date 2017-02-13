1e95db3bd12585b3d7b3ef4d6f41092ffe33e5ae
Jeff Eng <jeff@jeffeng.me>
2017-02-13 15:22:21 -0800

    Created the DropShadow protocol extension. Also created the HeaderView class that adopts the DropShadow protocol and its extension. Applied it to the header in storyboard via IBOutlet. Moved onto creating the Taco data model using a struct and enums for the different combinations.

--- Protocol Oriented Programming and Advanced Swift 3/TacoPOP/TacoPOP/DropShadow.swift
+++ Protocol Oriented Programming and Advanced Swift 3/TacoPOP/TacoPOP/DropShadow.swift
@@ -8,6 +8,14 @@
 
 import UIKit
 
-protocol DropShadow {
-    
+protocol DropShadow {}
+
+extension DropShadow where Self: UIView {
+    func addDropShadow() {
+        //Implementation
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.7
+        layer.shadowOffset = CGSize.zero
+        layer.shadowRadius = 5
+    }
 }
