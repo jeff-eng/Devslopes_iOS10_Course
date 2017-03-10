856b6792827295fc7f13bcead817864b4f96ea14
Jeff Eng <jeff@jeffeng.me>
2017-03-09 16:54:21 -0800

    Added pan gesture recognizer to PokemonSelectionVC in storyboard and IBAction to PokemonSelectionVC class. Calculate teh vertical drag distance and used a switch to trigger code based on the interactor state.

---
+++ Maps, GPS, Geolocation, Pokemon & Firebase/pokefinder/pokefinder/Interactor.swift
@@ -0,0 +1,14 @@
+//
+//  Interactor.swift
+//  pokefinder
+//
+//  Created by Jeffrey Eng on 3/9/17.
+//  Copyright © 2017 Jeffrey Eng. All rights reserved.
+//
+
+import UIKit
+
+class Interactor: UIPercentDrivenInteractiveTransition {
+    var hasStarted = false
+    var shouldFinish = false
+}
