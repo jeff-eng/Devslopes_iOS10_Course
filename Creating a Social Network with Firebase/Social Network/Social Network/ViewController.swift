1459cbfbdcfbc8e85a11d29c213e2c0869832af1
Jeff Eng <jeff@jeffeng.me>
2017-03-13 17:29:43 -0700

    Initial commit. Created Xcode project for Social Network app

---
+++ Creating a Social Network with Firebase/Social Network/Social Network/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Social Network
+//
+//  Created by Jeffrey Eng on 3/13/17.
+//  Copyright © 2017 Jeffrey Eng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
