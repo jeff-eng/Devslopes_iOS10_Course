08d26eb81a32706455e456f70e2249fdae7f761b
Jeff Eng <jeff@jeffeng.me>
2017-03-15 17:02:32 -0700

    Created custom classes for the UITextFields as well as the Sign In button. Assigned the classes to the elements in the storyboard. Tested and working.

---
+++ Creating a Social Network with Firebase/Social Network/Social Network/StyleTextField.swift
@@ -0,0 +1,29 @@
+//
+//  TextFieldPadding.swift
+//  Social Network
+//
+//  Created by Jeffrey Eng on 3/15/17.
+//  Copyright © 2017 Jeffrey Eng. All rights reserved.
+//
+
+import UIKit
+
+class StyleTextField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.borderColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.2).cgColor
+        layer.borderWidth = 1.0
+        layer.cornerRadius = 2.0
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+
+}
