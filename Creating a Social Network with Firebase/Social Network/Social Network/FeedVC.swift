1fe93e84cda4551d535f1de4212f2fb788e8bb73
Jeff Eng <jeff@jeffeng.me>
2017-03-20 16:19:31 -0700

    Created a new view controller for the social feed, added a segue between the two VCs.

---
+++ Creating a Social Network with Firebase/Social Network/Social Network/FeedVC.swift
@@ -0,0 +1,19 @@
+//
+//  FeedVC.swift
+//  Social Network
+//
+//  Created by Jeffrey Eng on 3/20/17.
+//  Copyright © 2017 Jeffrey Eng. All rights reserved.
+//
+
+import UIKit
+
+class FeedVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
