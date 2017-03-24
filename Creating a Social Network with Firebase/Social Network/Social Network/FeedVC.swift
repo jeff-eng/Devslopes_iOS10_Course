17a18364963969b00352d8ea7269e968078ad227
Jeff Eng <jeff@jeffeng.me>
2017-03-24 08:18:32 -0700

    Added the UITableViewDelegate and UITableViewDataSource protocols to the FeedVC. Implemented the required methods for UITableViewDataSource.

--- Creating a Social Network with Firebase/Social Network/Social Network/FeedVC.swift
+++ Creating a Social Network with Firebase/Social Network/Social Network/FeedVC.swift
@@ -10,14 +10,18 @@ import UIKit
 import SwiftKeychainWrapper
 import Firebase
 
-class FeedVC: UIViewController {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
-    @IBAction func signOutTapped(_ sender: UIButton) {
+    @IBAction func signOutPressed(_ sender: UIButton) {
         // Remove ID from keychain
         let keychainResult = KeychainWrapper.standard.removeObject(forKey: KEY_UID)
         print("Jeff: \(keychainResult)")
@@ -27,4 +31,12 @@ class FeedVC: UIViewController {
         dismiss(animated: true, completion: nil)
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
 }
