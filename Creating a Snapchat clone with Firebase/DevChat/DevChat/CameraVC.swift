831782be98088d8c29a9b216ec760687c75199b6
Jeff Eng <jeff@jeffeng.me>
2017-04-07 16:02:03 -0700

    Imported Apple sample code for the AVCam. Used Obj-C files.

---
+++ Creating a Snapchat clone with Firebase/DevChat/DevChat/CameraVC.swift
@@ -0,0 +1,20 @@
+//
+//  CameraVC.swift
+//  DevChat
+//
+//  Created by Jeffrey Eng on 4/7/17.
+//  Copyright © 2017 Jeffrey Eng. All rights reserved.
+//
+
+import UIKit
+
+class CameraVC: AVCamCameraViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
