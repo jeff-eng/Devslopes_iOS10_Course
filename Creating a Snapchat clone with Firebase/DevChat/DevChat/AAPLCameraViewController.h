550e11562c5a9b8df162c2087832f8e10d06edab
Jeff Eng <jeff@jeffeng.me>
2017-04-11 12:43:04 -0700

    Created delegate protocol for the AAPLCameraViewController to allow the Obj C code to work with the Swift code.

--- Creating a Snapchat clone with Firebase/DevChat/DevChat/AAPLCameraViewController.h
+++ Creating a Snapchat clone with Firebase/DevChat/DevChat/AAPLCameraViewController.h
@@ -8,9 +8,11 @@ View controller for camera interface.
 
 @import UIKit;
 @class AAPLPreviewView;
+@protocol AAPLCameraVCDelegate;
 
 @interface AAPLCameraViewController : UIViewController
 @property (nonatomic, weak) AAPLPreviewView *_previewView;
+@property (retain) id <AAPLCameraVCDelegate> delegate;
 
 // Adding files here to make it public facing
 - (void)toggleMovieRecording;
