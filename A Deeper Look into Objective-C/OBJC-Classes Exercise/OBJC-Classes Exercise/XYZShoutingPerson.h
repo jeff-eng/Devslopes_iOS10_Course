7eab740b0beb59c4ada9df3e4e0ed3897a849833
Jeff Eng <jeff@jeffeng.me>
2017-04-26 16:12:02 -0700

    Completed the exercise for the Working with Objects section of Apples Objective-C programming documentation.

---
+++ A Deeper Look into Objective-C/OBJC-Classes Exercise/OBJC-Classes Exercise/XYZShoutingPerson.h
@@ -0,0 +1,13 @@
+//
+//  XYZShoutingPerson.h
+//  OBJC-Classes Exercise
+//
+//  Created by Jeffrey Eng on 4/26/17.
+//  Copyright © 2017 Jeffrey Eng. All rights reserved.
+//
+
+#import "XYZPerson.h"
+
+@interface XYZShoutingPerson : XYZPerson
+
+@end
---
+++ A Deeper Look into Objective-C/OBJC-Classes Exercise/OBJC-Classes Exercise/XYZShoutingPerson.m
@@ -0,0 +1,13 @@
+//
+//  XYZShoutingPerson.m
+//  OBJC-Classes Exercise
+//
+//  Created by Jeffrey Eng on 4/26/17.
+//  Copyright © 2017 Jeffrey Eng. All rights reserved.
+//
+
+#import "XYZShoutingPerson.h"
+
+@implementation XYZShoutingPerson
+
+@end
